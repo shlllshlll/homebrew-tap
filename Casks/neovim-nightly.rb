000970b5819de762a72db22053eb855d5a1bc6be
shlll <shi.shl@alibaba-inc.com>
2020-07-05 21:29:52 +0800

    [Cask]修复Neovim链接并新增Neovim Nightly

--- Casks/neovim-nightly.rb
+++ Casks/neovim-nightly.rb
@@ -1,11 +1,11 @@
-cask 'neovim' do
-  version '0.4.3'
+cask 'neovim-nightly' do
+  version :latest
   sha256 :nocheck
 
   # github.com/neovim/neovim was verified as official when first introduced to the cask
-  url "https://github.com/neovim/neovim/releases/download/v#{version}/nvim-macos.tar.gz"
+  url "https://github.com/neovim/neovim/releases/download/nightly/nvim-macos.tar.gz"
   appcast 'https://github.com/neovim/neovim/releases.atom'
-  name 'Neovim'
+  name 'Neovim Nightly'
   homepage 'https://neovim.io/'
 
   binary 'nvim-osx64/bin/nvim'
